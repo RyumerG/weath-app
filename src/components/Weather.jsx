0dcb350219e487227bb3b3d61d95b8bc8af6a080
Peony Gerochi <itsduc@gmail.com>
2018-09-09 17:04:41 -0400

    o

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import { GetWeather } from '../services';
 
-const Weather = () => {
+class Weather extends Component {
 
-  GetWeather().then(data => {
-    console.log(data.data);
-  }).catch((error) => {
-    return error
-  });
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: '',
+    }
+  }
 
-  return (
-    <div>
+  componentDidMount() {
+    GetWeather().then(data => {
+      console.log(data.data);
+      this.setState({weather: data.data});
+    }).catch((error) => {
+      return error
+    });
+  }
 
-    </div>
-    )
-};
+  render() {
+    return (
+      <div>
+        {JSON.stringify(this.state.weather)}
+      </div>
+  );
+  }
+}
 
 export default Weather;
